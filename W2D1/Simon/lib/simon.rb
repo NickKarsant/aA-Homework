09956f92228c06368150db00e1563009fc51914c
Nick <nkarsant@gmail.com>
2019-01-14 18:48:56 -0800

    Set seq array to empty

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -6,16 +6,25 @@ class Simon
 	def initialize
 		@sequence_length = 1 
 		@game_over = false
-		@sequence = []
+		@seq = Array.new
   end
 
 	def play
-		
+		require_sequence
+		show_sequence
+
 		until game_over 
 			take_turn
+
+			round_success_message
+			add_random_color
+			@sequence_length += 1
+			
 		end
 		game_over_message 
 		reset_game
+	
+		
 
   end
 
@@ -27,26 +36,29 @@ class Simon
   end
 
   def show_sequence
-
+		add_random_color
   end
 
   def require_sequence
-
+		@seq
   end
 
   def add_random_color
-		@sequence << COLORS.sample
+		color = COLORS.sample
+		@seq << color
   end
 
   def round_success_message
-
+		puts "Success!"
   end
 
   def game_over_message
-
+		puts "Game Over"
   end
 
   def reset_game
-
+		@sequence_length = 1 
+		@game_over = false
+		@seq = Array.new
   end
 end
